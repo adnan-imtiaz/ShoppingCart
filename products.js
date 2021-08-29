eac9c055188a971f435e639a0180486d1e33c5b5
adnan-imtiaz <88797587+adnan-imtiaz@users.noreply.github.com>
2021-08-30 00:34:39 +0600

    Add files via upload

---
+++ products.js
@@ -0,0 +1,25 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+    123: {
+        name: "Redmi Note 10 pro",
+        desc: "XIOMI",
+        price: 23000
+    },
+    124: {
+        name: "Realme 8",
+        desc: "REALME",
+
+        price: 21050
+    },
+    125: {
+        name: "Samsung A21",
+        desc: "Samsung",
+        price: 16675
+    },
+    126: {
+        name: "Iphone X",
+        desc: "Apple ",
+
+        price: 110842
+    }
+};
